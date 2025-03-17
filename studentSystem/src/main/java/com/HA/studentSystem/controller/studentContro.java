99950852646772ad0b57515e2e9ed060be559a17
tuananh2005889 <tuannguyen.120910@gmail.com>
2025-03-18 01:03:21 +0700

    funtion add student

--- studentSystem/src/main/java/com/HA/studentSystem/controller/studentContro.java
+++ studentSystem/src/main/java/com/HA/studentSystem/controller/studentContro.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.HA.studentSystem.model.student;
 import com.HA.studentSystem.service.studentService;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin(origins = "http://localhost:3000")
 public class studentContro {
 
   @Autowired
